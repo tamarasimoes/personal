6387fdc4a10f528ba4ac18940e21ad2fadd0166e
Tamara Simões <tamara.simoes@betrybe.com>
2022-01-12 11:43:21 -0300

    animações 2

---
+++ scripts.js
@@ -0,0 +1,9 @@
+$('.nav a[href^="#contact"]').on('click', function(e) {
+	e.preventDefault();
+	var id = $(this).attr('href'),
+			targetOffset = $(id).offset().top;
+			
+	$('html, body').animate({ 
+		scrollTop: targetOffset - 100
+	}, 500);
+});
